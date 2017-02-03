441da410f5e0b349aeb64c23a24a2d167babd04b
Turnt-SNACO <janderson5@albany.edu>
2017-02-02 19:05:56 -0500

    Armor class finished and working.

--- HW1/AndersonJamesH1/src/def/Armor.java
+++ HW1/AndersonJamesH1/src/def/Armor.java
@@ -1,10 +1,14 @@
 package def;
 
 public class Armor extends Item{
-
-	public Armor(String name, int weight) {
-		super(name, weight);
-
+	private int defense;
+	public Armor(String name, int weight, int defense) {
+		super(name, weight);	this.defense=defense;
 	}
-
+	public int getDefense(){
+		return defense;
+	}
+	public String examine(){
+		return ("This is a "+super.getName()+" with weight "+super.getWeight()+" and defense "+defense+". It is Armor.");
+	}
 }
--- HW1/AndersonJamesH1/src/def/Main.java
+++ HW1/AndersonJamesH1/src/def/Main.java
@@ -6,8 +6,12 @@ public class Main {
 		//Object testing
 		Weapon mace = new Weapon("mace", 5, 10);
 		Item rope = new Item("rope", 2);
+		Armor chain = new Armor("Chainmail", 10, 5);
 		System.out.println(mace.examine());
 		System.out.println(rope.examine());
+		System.out.println(mace.getWeight());
+		System.out.println(chain.examine());
+		System.out.println(chain.getName());
 
 	}
 
