7973acc54ec86e3e934586b6f35e7137377bd7db
Turnt-SNACO <janderson5@albany.edu>
2017-02-02 19:13:43 -0500

    Food class done. Most likely final commit.

--- HW1/AndersonJamesH1/src/def/Food.java
+++ HW1/AndersonJamesH1/src/def/Food.java
@@ -1,10 +1,21 @@
 package def;
 
 public class Food extends Item{
-
-	public Food(String name, int weight) {
-		super(name, weight);
-		
+	private int nutrition, quantity;
+	public Food(String name, int weight, int nutrition, int quantity) {
+		super(name, weight);	this.nutrition=nutrition;	this.quantity=quantity;
+	}
+	public void setQuantity(int newQuantity){
+		this.quantity=newQuantity;
+	}
+	public int getQuantity(){
+		return quantity;
+	}
+	public int getNutrition(){
+		return nutrition;
+	}
+	public String examine(){
+		return ("This is a(n) "+super.getName()+" with weight "+super.getWeight()+" and nutrition value "+ nutrition+". There are "+quantity+" in your inventory.");
 	}
 
 }
--- HW1/AndersonJamesH1/src/def/Main.java
+++ HW1/AndersonJamesH1/src/def/Main.java
@@ -7,11 +7,14 @@ public class Main {
 		Weapon mace = new Weapon("mace", 5, 10);
 		Item rope = new Item("rope", 2);
 		Armor chain = new Armor("Chainmail", 10, 5);
+		Food apple = new Food("apple", 1, 2, 4);
 		System.out.println(mace.examine());
 		System.out.println(rope.examine());
 		System.out.println(mace.getWeight());
 		System.out.println(chain.examine());
 		System.out.println(chain.getName());
+		System.out.println(apple.examine());
+		System.out.println(apple.getName());
 
 	}
 
