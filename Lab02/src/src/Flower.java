bbeb7d08d53a6a9e0a443a8ef433574b791caec7
Turnt-SNACO <janderson5@albany.edu>
2017-02-07 11:59:10 -0500

    Initial commit for Lab02

---
+++ Lab02/src/src/Flower.java
@@ -0,0 +1,17 @@
+package src;
+
+public class Flower {
+	private String name, color;
+	//Constructor
+	public Flower(String name, String color){
+		this.name=name;		this.color=color;
+	}
+	//Returns name as a string
+	public String getName() {
+		return name;
+	}
+	//Returns color as a string
+	public String getColor() {
+		return color;
+	}	
+}
