70d7a659b7cae96ad0ce1f258d7e2a8351e0b695
Turnt-SNACO <janderson5@albany.edu>
2017-02-14 14:43:32 -0500

    Commit "test"

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,11 +1,21 @@
-
 public class Main {
-
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("HEY!");
-		System.out.println("BYE");
+	//this exists so when calling the recursive function only one argument needs to be passed.
+	public static double root(double initial){
+		return rootRecursion(initial,initial);
 	}
-
+	//actual recursive function to find the root of the given number initial
+	public static double rootRecursion(double initial, double Guess){
+		double guess=Guess/2;
+		// exact answer					//acceptable error margin
+		if (guess*guess==initial  || (initial-guess*guess< 0.0000000000001 && initial-guess*guess>-0.0000000000001))
+			return guess;
+		//generate next guess
+		double nextGuess = guess+initial/guess;
+		//call next guess
+		return rootRecursion(initial, nextGuess);
+	}
+	//Test
+	public static void main(String[] args) {
+		System.out.println(root(54912));
+	}
 }
