a85430543f0eb5e0514419219a94b2733805d233
Turnt-SNACO <janderson5@albany.edu>
2017-02-26 19:47:06 -0500

    Created Babylonian Sqrt menthod with error margin of +/- 0.01.  Still requires user to input guess along with the number being rooted.

--- AndersonJamesH2/src/Sqrt.java
+++ AndersonJamesH2/src/Sqrt.java
@@ -1,5 +1,14 @@
 
 public class Sqrt {
 	Sqrt(){}
-	
+	public double rootRecursion(double initial, double Guess){
+		double guess=Guess/2;
+		// exact answer					//acceptable error margin
+		if (guess*guess==initial  || (initial-guess*guess< 0.01 && initial-guess*guess>-0.01))
+			return guess;
+		//generate next guess
+		double nextGuess = guess+initial/guess;
+		//call next guess
+		return rootRecursion(initial, nextGuess);
+	}
 }
