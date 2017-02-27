5f3a9a45fe1cb4e096e1fe2c8366e645cc884150
Turnt-SNACO <janderson5@albany.edu>
2017-02-26 19:54:26 -0500

    All square roots returning properly (checked against calculator). Final commit.

--- AndersonJamesH2/src/Main.java
+++ AndersonJamesH2/src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
-
+		Sqrt s = new Sqrt();
+		System.out.println("Perfect squares:");
+		System.out.println("Sqrt(25) = "+s.root(25));
+		System.out.println("Sqrt(4) = "+s.root(4));
+		System.out.println("Sqrt(729) = "+s.root(729));
+		System.out.println("Non-perfect Squares:");
+		System.out.println("Sqrt(30) = "+s.root(30));
+		System.out.println("Sqrt(50) = "+s.root(50));
+		System.out.println("Sqrt(3412) = "+s.root(3412));
 	}
 
 }
