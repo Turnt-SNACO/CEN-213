5219b2a07448dc6b56c07c4725fd7a52594dc2e8
Turnt-SNACO <janderson5@albany.edu>
2017-02-26 19:48:41 -0500

    Adjusted error margin to accomodate large numbers sufficient for this program while having known roots coming out completely accurate.  i.e. sqrt of 4 will give exact 2

--- AndersonJamesH2/src/Sqrt.java
+++ AndersonJamesH2/src/Sqrt.java
@@ -1,10 +1,10 @@
 
 public class Sqrt {
 	Sqrt(){}
-	public double rootRecursion(double initial, double Guess){
+	private double rootRecursion(double initial, double Guess){
 		double guess=Guess/2;
 		// exact answer					//acceptable error margin
-		if (guess*guess==initial  || (initial-guess*guess< 0.01 && initial-guess*guess>-0.01))
+		if (guess*guess==initial  || (initial-guess*guess< 0.0000000000001 && initial-guess*guess>-0.0000000000001))
 			return guess;
 		//generate next guess
 		double nextGuess = guess+initial/guess;
