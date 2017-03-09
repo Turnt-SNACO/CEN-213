fafb033cbb3b7c12ecc571fb81a97c071f4601ef
Turnt-SNACO <janderson5@albany.edu>
2017-03-09 14:35:03 -0500

    Updated tests.

--- AndersonJamesH3/src/Main.java
+++ AndersonJamesH3/src/Main.java
@@ -1,14 +1,24 @@
 public class Main {
 	public static void main(String[]args){
 		LinkedList l = new LinkedList();
-		Node apple = new Node("apple");
 		Node ace = new Node("ace");
-		Node xylophone = new Node("xylophone");
+		Node apple = new Node("apple");
 		Node bubblegum = new Node("bubblegum");
+		Node xylophone = new Node("xylophone");
+		Node zebra = new Node("zebra");
 		l.insertAlphabetically(apple);
 		l.insertAlphabetically(xylophone);
 		l.insertAlphabetically(bubblegum);
 		l.insertAlphabetically(ace);
+		System.out.println(l.printForward());
+		System.out.println(l.printReverse());
+		l.deleteNode(bubblegum);
+		System.out.println(l.printForward());
+		System.out.println(l.printReverse());
+		l.insertAlphabetically(zebra);
+		System.out.println(l.printForward());
+		System.out.println(l.printReverse());
+		l.addToEnd(new Node(0));
 		System.out.println(l.printForward());
 		System.out.println(l.printReverse());
 	}
